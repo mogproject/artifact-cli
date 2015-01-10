c7e6b22952af2c549045b5fcdf3a70ca6cdc317e
Yosuke Mizutani <mogproj@gmail.com>
2015-01-10 14:21:09 +0900

    version 0.1.3

--- src/artifactcli/argparser.py
+++ src/artifactcli/argparser.py
@@ -1,6 +1,6 @@
 from optparse import OptionParser
 
-VERSION = 'artifact-cli 0.1.2'
+VERSION = 'artifact-cli 0.1.3'
 USAGE = """
   %prog [options] list     GROUP
   %prog [options] upload   GROUP LOCAL_PATH
