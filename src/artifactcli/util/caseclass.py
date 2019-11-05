ed97bc7f1fe7e8902c698b72b57539d5cb371013
arcizan <ghostrevery@gmail.com>
2019-11-05 21:58:09 +0900

    implement rich comparison methods

--- src/artifactcli/util/caseclass.py
+++ src/artifactcli/util/caseclass.py
@@ -1,3 +1,8 @@
+from functools import total_ordering
+from collections import Hashable
+
+
+@total_ordering
 class CaseClass(object):
     """
     Implementation like Scala's case class
@@ -9,18 +14,46 @@ class CaseClass(object):
         """
         self.__keys = keys
 
-    def __cmp__(self, other):
+    def __eq__(self, other):
         if not isinstance(other, self.__class__):
             # compare with class names
-            return cmp(self.__class__.__name__, other.__class__.__name__)
+            return self.__class__.__name__ == other.__class__.__name__
 
         for k in self.__keys:
             a, b = getattr(self, k), getattr(other, k)
-            if a < b:
-                return -1
-            if a > b:
-                return 1
-        return 0
+            if a != b:
+                return False
+        return True
+
+    def __lt__(self, other):
+        if not isinstance(other, self.__class__):
+            # compare with class names
+            return self.__class__.__name__ < other.__class__.__name__
+
+        for k in self.__keys:
+            a, b = getattr(self, k), getattr(other, k)
+            try:
+                # None is always less than any datatype
+                if a is None and b is not None:
+                    return True
+                elif a is not None and b is None:
+                    return False
+                elif a < b:
+                    return True
+                elif a > b:
+                    return False
+            except TypeError:
+                a, b = self.__to_comparable(a), self.__to_comparable(b)
+                if a < b:
+                    return True
+                elif a > b:
+                    return False
+        return False
 
     def __repr__(self):
         return '%s(%s)' % (self.__class__.__name__, ', '.join('%s=%r' % (k, getattr(self, k)) for k in self.__keys))
+
+    def __to_comparable(self, value):
+        if isinstance(value, Hashable):
+            return hash(value)
+        return repr(value)
